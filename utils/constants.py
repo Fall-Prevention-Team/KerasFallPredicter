59bf8bf3545a4cd0eaa735edce47b2a31a291966
Sponsored_By_Redbull <redbull@pop-os.localdomain>
2022-05-02 12:07:37 +0200

    new models: fullmod**48

--- utils/constants.py
+++ utils/constants.py
@@ -17,7 +17,7 @@
                             'uWaveGestureLibrary_Z', 'wafer', 'Wine', 'WordsSynonyms', 'Worms', 'WormsTwoClass', 'yoga']
 """
 # UNIVARIATE_DATASET_NAMES = ['Meat', 'Coffee']
-UNIVARIATE_DATASET_NAMES = ['fullfix48']
+UNIVARIATE_DATASET_NAMES = ['fullmod48', 'fullmodsplit48']
 
 UNIVARIATE_ARCHIVE_NAMES = ['TSC']
 # UNIVARIATE_ARCHIVE_NAMES = ['TSC', 'InlineSkateXPs', 'SITS']
